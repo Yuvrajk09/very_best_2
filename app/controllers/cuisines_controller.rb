1d9dba20ba4ae545a969ce7a5f337ecdb9aed15c
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-28 00:33:47 +0000

    Remove controller comments

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,24 +1,19 @@
 class CuisinesController < ApplicationController
   before_action :set_cuisine, only: %i[show edit update destroy]
 
-  # GET /cuisines
   def index
     @q = Cuisine.ransack(params[:q])
     @cuisines = @q.result(distinct: true).includes(:dish).page(params[:page]).per(10)
   end
 
-  # GET /cuisines/1
   def show; end
 
-  # GET /cuisines/new
   def new
     @cuisine = Cuisine.new
   end
 
-  # GET /cuisines/1/edit
   def edit; end
 
-  # POST /cuisines
   def create
     @cuisine = Cuisine.new(cuisine_params)
 
@@ -34,7 +29,6 @@ class CuisinesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cuisines/1
   def update
     if @cuisine.update(cuisine_params)
       redirect_to @cuisine, notice: "Cuisine was successfully updated."
@@ -43,7 +37,6 @@ class CuisinesController < ApplicationController
     end
   end
 
-  # DELETE /cuisines/1
   def destroy
     @cuisine.destroy
     message = "Cuisine was successfully deleted."
@@ -56,12 +49,10 @@ class CuisinesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_cuisine
     @cuisine = Cuisine.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def cuisine_params
     params.require(:cuisine).permit(:name, :dish_id)
   end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,25 +1,20 @@
 class VenuesController < ApplicationController
   before_action :set_venue, only: %i[show edit update destroy]
 
-  # GET /venues
   def index
     @q = Venue.ransack(params[:q])
     @venues = @q.result(distinct: true).includes(:famous_dish,
                                                  :user).page(params[:page]).per(10)
   end
 
-  # GET /venues/1
   def show; end
 
-  # GET /venues/new
   def new
     @venue = Venue.new
   end
 
-  # GET /venues/1/edit
   def edit; end
 
-  # POST /venues
   def create
     @venue = Venue.new(venue_params)
 
@@ -35,7 +30,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /venues/1
   def update
     if @venue.update(venue_params)
       redirect_to @venue, notice: "Venue was successfully updated."
@@ -44,7 +38,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # DELETE /venues/1
   def destroy
     @venue.destroy
     message = "Venue was successfully deleted."
@@ -57,12 +50,10 @@ class VenuesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_venue
     @venue = Venue.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def venue_params
     params.require(:venue).permit(:dish_id, :venue_name)
   end
