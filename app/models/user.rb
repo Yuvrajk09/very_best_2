9915cefc9661bca2a3b612668d42c50420e92774
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-28 00:32:35 +0000

    Direct association between users and dishes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :favorite_dishes,
+             :class_name => "Dish",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
